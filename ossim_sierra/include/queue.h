21a4239229aa9f712db32a2e76bdf91ea6280558
iAmHuyyy <ngnhathuy1305@gmail.com>
2025-04-12 00:44:08 +0700

    Update queue.h part 1

--- ossim_sierra/include/queue.h
+++ ossim_sierra/include/queue.h
@@ -9,6 +9,7 @@
 struct queue_t {
 	struct pcb_t * proc[MAX_QUEUE_SIZE];
 	int size;
+	int slot;
 };
 
 void enqueue(struct queue_t * q, struct pcb_t * proc);
@@ -18,4 +19,6 @@ struct pcb_t * dequeue(struct queue_t * q);
 int empty(struct queue_t * q);
 
 #endif
+
+
 
